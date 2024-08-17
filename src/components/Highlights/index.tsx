bab6ef9cf1e2f6d5992704a643e5591b2a502f9f
Daniel Santos <danieldiefsantos@gmail.com>
2024-08-17 15:32:31 -0300

    fix: tipagem api + arquivo renomeado

--- src/components/Highlights/index.tsx
+++ src/components/Highlights/index.tsx
@@ -1,13 +1,14 @@
 import SmallFilmBox from "../SmallFilmBox";
 import highlightsRequest from "../../common/requests/highlightsRequest";
 import { useEffect, useState } from "react";
-import IfilmHighlight from "../../common/types/IfilmHighlight";
+import IFilmHighlight from "../../common/types/IFilmHighlight";
+import SlideHighlights from "../SlideHighlights";
 
 //Estava com dificuladade de chamar a função assincrona para o elemento
 //sem que tonasse o Highlights asyncrono, por isso, usei o ChatGPT
 
 export default function Highlights() {
-    const [films, setFilms] = useState(Array<IfilmHighlight>);
+    const [films, setFilms] = useState(Array<IFilmHighlight>);
 
     useEffect(() => {
         (async () => {
@@ -19,11 +20,11 @@ export default function Highlights() {
     return (
         <>
             {
-                films.map((film, key) => (
+                films.slice(1).map((film, key) => (
                     <SmallFilmBox
                         key={key}
                         id={film.id}
-                        title={film.title}
+                        name={film.name}
                         vote_average={film.vote_average}
                         backdrop_path={film.backdrop_path}
                     />

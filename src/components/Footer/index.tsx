588649b0d6858969d1d91b0b9a1bf556b0333697
Daniel Santos <danieldiefsantos@gmail.com>
2024-08-17 11:05:42 -0300

    feat: criação do Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import { FooterDiv, FooterLabel } from "./styles";
+
+export default function Footer() {
+    return (
+        <FooterDiv>
+            <FooterLabel>
+                &copy; 2024 Rader. All rights reserved
+            </FooterLabel>
+        </FooterDiv>
+    )
+}
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const FooterDiv = styled.footer`
+    width: 1440px;
+    height: 67px;
+    padding: 24px;
+    gap: 12px;
+    margin: auto;
+    position: fixed;
+    bottom: 0;
+`
+
+export const FooterLabel = styled.p`
+    width: 253px;
+    height: 19px;
+    font-family: 'Inter', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19.36px;
+    color: #B4B4B4;
+    margin: auto;
+`

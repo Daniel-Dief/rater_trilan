0e4c045e149d3ee0d1c1f400efe60adc1533c861
Daniel Santos <danieldiefsantos@gmail.com>
2024-08-17 15:02:55 -0300

    fix: caminho das imagens

--- src/components/SmallFilmBox/index.tsx
+++ src/components/SmallFilmBox/index.tsx
@@ -7,14 +7,14 @@ export default function SmallFilmBox({ id, title, vote_average, backdrop_path } 
     return (
         <CardMovie background={backgroundUrl}>
             <StarsBox>
-                <Icon src={require("../../../assets/images/star.png")} alt="Star"/>
+                <Icon src={require("../../assets/images/star.png")} alt="Star"/>
                 <p>{Math.round(vote_average * 10) / 10}</p>
             </StarsBox>
             <DivBottom>
                 <Title>{title}</Title>
                 <TrailerButton>
                     <p>Assistir ao trailer</p>
-                    <Icon src={require("../../../assets/images/play.png")} alt="Play" />
+                    <Icon src={require("../../assets/images/play.png")} alt="Play" />
                 </TrailerButton>
             </DivBottom>
         </CardMovie>

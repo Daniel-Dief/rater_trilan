4d184ddc942e5815f0116b8e54150f2ebfd4072d
Daniel Santos <danieldiefsantos@gmail.com>
2024-08-18 17:55:28 -0300

    feat: Tipagem do catalogo

---
+++ src/common/types/ICatalogue.ts
@@ -0,0 +1,9 @@
+export default interface ICatalogue {
+    backdrop_path: string
+    vote_average: number
+    character: string
+    title?: string
+    name?: string
+    release_date?: string
+    first_air_date?: string
+}

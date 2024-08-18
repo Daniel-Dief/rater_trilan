c3dcf54961a25ab884ec26245af8ba1b8c8e19ab
Daniel Santos <danieldiefsantos@gmail.com>
2024-08-18 20:56:50 -0300

    feat: Adicionando banner do filme

--- src/pages/Movie/index.tsx
+++ src/pages/Movie/index.tsx
@@ -3,18 +3,39 @@ import IPeopleSummary from "../../common/types/IPeopleSummary"
 import IFilmSummary from "../../common/types/IFilmSummary"
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
+import BannerMovie from "../../components/BannerMovie"
 import Slicer from "../../components/Slider"
 import castRequest from "../../common/requests/castRequest"
 import likeMoviesRequest from "../../common/requests/likeMoviesRequest"
+import movieDetails from "../../common/requests/movieDetails"
 
 import { Main } from "./styles"
+import IFilmDetails from "../../common/types/IFilmDetails"
+
+const defaultMovie : IFilmDetails = {
+    id: 1,
+    title: "default",
+    overview: "default",
+    backdrop_path: "default",
+    vote_average: 1,
+    vote_count: 1,
+    genres: [],
+    release_date: new Date(),
+    runtime: 1,
+    adult: false,
+}
 
 export default function Movie() {
     const params = Number(new URLSearchParams(window.location.search).get("id"));
+    const [movie, setMovie] = useState<IFilmDetails>();
     const [cast, setCast] = useState<Array<IPeopleSummary>>([])
     const [likeMovies, setLikeMovies] = useState<Array<IFilmSummary>>([])
 
     useEffect(() => {
+        (async () => {
+            const tempMovie = await movieDetails(params);
+            setMovie(tempMovie);
+        })();
         (async () => {
             const tempCast = await castRequest(params);
             setCast(tempCast);
@@ -28,6 +49,9 @@ export default function Movie() {
     return (
         <>
             <Header />
+            <BannerMovie
+                movie={movie ?? defaultMovie}
+            />
             <Main>
                 <Slicer
                     title="Elenco principal"

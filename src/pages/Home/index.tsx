1607ee3b1b2accdd666553b63c81fa0d48bf9b5e
Daniel Santos <danieldiefsantos@gmail.com>
2024-08-18 10:58:53 -0300

    feat: Adicionando Slider de lançamentos na Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,29 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Highlights from "../../components/Highlights";
+import Slider from "../../components/Slider";
+import { useEffect, useState } from "react";
+import IFilmSummary from "../../common/types/IFilmSummary";
+import releasesRequest from "../../common/requests/releasesRequest";
 
 export default function Home() {
+    const [releases, setReleases] = useState<Array<IFilmSummary>>([]);
+
+    useEffect(() => {
+        (async () => {
+            const films = await releasesRequest();
+            setReleases(films);
+        })();
+    }, []);  
+
     return (
         <>
             <Header />
             <Highlights />
+            <Slider
+                title="Ultimos lançamentos"
+                arrFilms={releases}
+            />
             <Footer />
         </>
     );

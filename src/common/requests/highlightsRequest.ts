0b7b49492412bfa3e83c0331e25d5aad6c390633
Daniel Santos <danieldiefsantos@gmail.com>
2024-08-18 08:05:10 -0300

    refactor: Utilizando o .env parar acessar a chave da API

--- src/common/requests/highlightsRequest.ts
+++ src/common/requests/highlightsRequest.ts
@@ -4,7 +4,7 @@ export default async function highlightsRequest() {
         method: 'GET',
         headers: {
             accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzhkOGQ1ZjdhYTg4YmE5ZWFlY2VjYzIyZDAzNjU0YyIsIm5iZiI6MTcyMzkwNTUwNC4yOTk3OTEsInN1YiI6IjY2YmY2ZGRjNGNiYjg0MjIyYzlkYTFhNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TTSQXSi0Kj0tcUWVH0ge2x6LWChzeHJAVz1U0efOehk'
+            Authorization: process.env.REACT_APP_API_KEY ? process.env.REACT_APP_API_KEY : ""
         }
     };
 

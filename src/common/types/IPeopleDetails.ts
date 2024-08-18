cae69afb3ae0cfcce13847b6cbb94198d10cd5ce
Daniel Santos <danieldiefsantos@gmail.com>
2024-08-18 15:56:52 -0300

    refactor: Ajuste de tipagens

--- src/common/types/IPeopleDetails.ts
+++ src/common/types/IPeopleDetails.ts
@@ -1,11 +1,11 @@
 import IFilmSummary from "./IFilmSummary"
-import ISerieSummary from "./ISerieSummary"
 
 export default interface IPeopleDetails {
     name: string
     birthday: string
+    deathday: string
     place_of_birth: string
     profile_path: string
     known_for_department: string
-    movies_and_series: Array<IFilmSummary | ISerieSummary>
+    biography: string
 }
--- src/common/types/ISerieSummary.ts
+++
@@ -1,6 +0,0 @@
-export default interface ISerieSummary {
-    backdrop_path: string
-    name: string
-    vote_average: number
-    first_air_date:string
-}

02c9a351a2f73793bdba18176f25f0e01b3239d9
Daniel Santos <danieldiefsantos@gmail.com>
2024-08-18 10:57:44 -0300

    feat: Criando request dos lançamentos

---
+++ src/common/requests/releasesRequest.ts
@@ -0,0 +1,24 @@
+export default async function highlightsRequest() {    
+    const url = process.env.REACT_APP_API_URL + 'movie/now_playing?language=pt-br&page=1&region=br';
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: process.env.REACT_APP_API_KEY ? process.env.REACT_APP_API_KEY : ""
+        }
+    };
+
+    let resJson = await fetch(url, options)
+    .then(async(data) => {
+        return data.json()
+        .then(data => data.results);
+    })
+    .catch(err => {
+        console.error(err)
+        return new Array<Object>
+    })
+
+    console.log(url);
+
+    return resJson;
+}

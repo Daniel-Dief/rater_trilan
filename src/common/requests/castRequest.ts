675932e5e583cc3fac7511161c283f280f1612a0
Daniel Santos <danieldiefsantos@gmail.com>
2024-08-18 19:28:09 -0300

    feat: Criando request do elenco

---
+++ src/common/requests/castRequest.ts
@@ -0,0 +1,21 @@
+export default async function castRequest(id : number) {    
+    const url = process.env.REACT_APP_API_URL + `movie/${id}/credits?language=pt-br`;
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: process.env.REACT_APP_API_KEY ? process.env.REACT_APP_API_KEY : ""
+        }
+    };
+
+    let resJson = await fetch(url, options)
+    .then(async(data) => {
+        return data.json()
+        .then(data => data.cast)
+    })
+    .catch(err => {
+        console.error(err)
+    })
+    
+    return resJson;
+}

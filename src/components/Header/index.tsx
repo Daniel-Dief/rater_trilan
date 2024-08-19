fbe279e1232ad5177f1d6abcbd6e4baa75e46d49
Daniel Santos <danieldiefsantos@gmail.com>
2024-08-19 20:02:43 -0300

    fix: Click na caixa de resultado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,9 @@ import ResultBox from "../ResultBox";
 export default function Header(){
     const [resultSearch, setResultSearch] = useState<Array<ISearch>>([]);
     const [inputFocus, setInputFocus] = useState<boolean>(false);
+    const [slicerFocus, setSlicerFocus] = useState<boolean>(false);
     const searchInput = useRef<HTMLInputElement>(null);
+    const containerRef = useRef<HTMLDivElement>(null);
 
     function redirectHome() {
         window.location.href = `/home`;
@@ -17,12 +19,11 @@ export default function Header(){
         setInputFocus(true)
     }
 
-    function onInputBlur() {
-        setTimeout(() => {
-            setInputFocus(false)        
-        }, 10)
-    }
-
+    function handleBlur (event: React.FocusEvent<HTMLInputElement>) {
+        if (containerRef.current && !containerRef.current.contains(event.relatedTarget as Node)) {
+            setInputFocus(false)
+        }
+    };
 
     function search() {
         if(inputFocus){
@@ -42,7 +43,7 @@ export default function Header(){
                         <LensDiv>
                             <Image src={require("../../assets/images/search.png")} alt="Lupa" />
                         </LensDiv>
-                        <InputSearch onFocus={onInputFocus} onBlur={onInputBlur} onChange={search} ref={searchInput} type="text" placeholder="Pesquisar..." />
+                        <InputSearch onFocus={onInputFocus} onBlur={handleBlur} onChange={search} ref={searchInput} type="text" placeholder="Pesquisar..." />
                     </SearchCamp>
                     <SlicersDiv>
                         <Image src={require("../../assets/images/sliders-horizontal.png")} alt="Filtros" />
@@ -51,7 +52,7 @@ export default function Header(){
                 {
                     resultSearch.length >= 1 && inputFocus
                     ?
-                    <DivFloat>
+                    <DivFloat ref={containerRef}>
                         <ResultBox
                             results={resultSearch}
                         />
